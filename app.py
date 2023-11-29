8394a9a0b247bca7fbbf80426eafa97d0f4d65da
Fajarbaluch63 <123570704+Fajarbaluch63@users.noreply.github.com>
2023-11-29 14:09:23 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,83 +1,99 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template
 from flask_mysqldb import MySQL
 from flask_cors import CORS
+import json
 
-app = Flask(__name__)
+mysql = MySQL()
+app = Flask(_name_)
 CORS(app)
 
-# MySQL configurations
+# MySQL Instance configurations
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'secret'
 app.config['MYSQL_DB'] = 'student'
-app.config['MYSQL_HOST'] = '34.147.154.231'
+app.config['MYSQL_HOST'] = '34.142.78.16'
+mysql.init_app(app)
 
-mysql = MySQL(app)
-
-# Function to execute queries safely
-def execute_query(query, params=None):
+def execute_query(query):
     try:
         cur = mysql.connection.cursor()
-        if params:
-            cur.execute(query, params)
-        else:
-            cur.execute(query)
+        print("Executing query:", query)
+        cur.execute(query)
         mysql.connection.commit()
+        print("Query executed successfully")
         return True
     except Exception as e:
-        app.logger.error(f"Error executing query: {e}")
+        print("Error:", e)
         return False
 
-# Routes
-@app.route("/add", methods=['POST'])
-def add_student():
-    data = request.get_json()
-    name = data.get('name')
-    email = data.get('email')
-    query = "INSERT INTO students(studentName, email) VALUES(%s, %s);"
-    success = execute_query(query, (name, email))
-    if success:
-        return jsonify(Result="Success")
-    else:
-        return jsonify(Result="Error"), 500
+@app.route("/add", methods=['POST'])  # Add Student
+def add():
+    name = request.json.get('name')
+    email = request.json.get('email')
+    try:
+        query = '''INSERT INTO students(studentName, email) VALUES('{}', '{}');'''.format(name, email)
+        success = execute_query(query)
 
-@app.route("/update", methods=['PUT'])
-def update_student():
-    data = request.get_json()
-    student_id = data.get('id')
-    name = data.get('name')
-    email = data.get('email')
-    query = "UPDATE students SET studentName = %s, email = %s WHERE studentID = %s;"
-    success = execute_query(query, (name, email, student_id))
-    if success:
-        return jsonify(Result="Success")
-    else:
-        return jsonify(Result="Error"), 500
+        if success:
+            return '{"Result": "Success"}'
+        else:
+            return '{"Result": "Error"}'
+    except Exception as e:
+        return '{"Result": "Error"}'
 
-@app.route("/delete", methods=['DELETE'])
-def delete_student():
-    name = request.get_json().get('name')
-    query = "DELETE FROM students WHERE studentName=%s;"
-    success = execute_query(query, (name,))
-    if success:
-        return jsonify(Result="Success")
-    else:
-        return jsonify(Result="Error"), 500
+@app.route("/update", methods=['PUT'])  # Update Student
+def update():
+    id = int(request.json.get('id'))
+    name = request.json.get('name')
+    email = request.json.get('email')
+    try:
+        query = '''UPDATE students SET studentName = '{}', email = '{}' WHERE studentID = {} ;'''.format(name, email, id)
+        print("Received Update Request. ID:", id, "Name:", name, "Email:", email)
+        success = execute_query(query)
+        print(success)
+        return '{"Result": "Success"}'
+    except Exception as e:
+        return '{"Result": "Error"}'
 
-@app.route("/default")
-def list_students():
+@app.route("/delete", methods=['DELETE'])  # Delete Student
+def delete():
+    name = request.json.get('name')
+    try:
+        query = '''DELETE FROM students WHERE studentName='{}';'''.format(name)
+        success = execute_query(query)
+        print(success)
+        return '{"Result": "Success"}'
+    except Exception as e:
+        return '{"Result": "Error"}'
+
+
+@app.route("/default")  # Default - Show Data
+def read():
     try:
         cur = mysql.connection.cursor()
-        cur.execute("SELECT * FROM students")
-        rows = cur.fetchall()
-        students = [{"ID": row[2], "Name": row[0], "Email": row[1]} for row in rows]
-        return jsonify(Results=students, count=len(students))
+        cur.execute('''SELECT * FROM students''')
+        rv = cur.fetchall()
+        Results = []
+        for row in rv:
+            Result = {}
+            Result['Name'] = row[0].replace('\n', ' ')
+            Result['Email'] = row[1]
+            Result['ID'] = row[2]
+            Results.append(Result)
+        response = {'Results': Results, 'count': len(Results)}
+        ret = app.response_class(
+            response=json.dumps(response),
+            status=200,
+            mimetype='application/json'
+        )
+        return ret
     except Exception as e:
-        app.logger.error(f"Error retrieving students: {e}")
-        return jsonify(Result="Error", Message="Could not retrieve students."), 500
-
+        return '{"Result": "Error"}'
+    
 @app.route('/')
 def index():
     return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+
+if _name_ == "_main_":
+    app.run(host='0.0.0.0', port='8080')
